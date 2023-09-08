4c4b05e7cdf32ff506abb92fe4675eda492f4d69
James McCullough <james.mccullough@aaoglobal.com>
2023-09-09 00:43:44 +0100

    feat: start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Box, Input, VStack } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+import {
+  AppContainer,
+  FaceBackImage,
+  FaceInnerBackImage,
+  FaceInnerCoverImage,
+  FaceInnerFrontCoverImage,
+  FaceInnerFrontImage,
+  FaceLayersContainer,
+  FaceOuterCoverImage,
+  InnerFaceLayersContainer,
+  OuterFaceLayersContainer,
+} from "./animationAndImageFormatting";
 
 function App() {
+  const [hours, setHours] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [seconds, setSeconds] = useState("");
+
+  const padNumber = (num) => {
+    return String(num).padStart(3, "0");
+  };
+
+  const playTimeUnitSound = (num, unit, callback) => {
+    let audio;
+    if (num >= 1 && num <= 19) {
+      audio = new Audio(`SFX/Glados_Number_${padNumber(num)}.mp3`);
+    } else {
+      let tens = Math.floor(num / 10) * 10;
+      let ones = num % 10;
+
+      if (tens !== 0 && ones !== 0) {
+        audio = new Audio(`SFX/Glados_Number_${padNumber(tens)}.mp3`);
+        audio.onended = () => {
+          const onesAudio = new Audio(
+            `SFX/Glados_Number_${padNumber(ones)}.mp3`
+          );
+          onesAudio.onended = callback; // Call the callback after ones sound has ended
+          onesAudio.play();
+        };
+      } else if (tens !== 0) {
+        audio = new Audio(`SFX/Glados_Number_${padNumber(tens)}.mp3`);
+        audio.onended = callback;
+      } else if (ones !== 0) {
+        audio = new Audio(`SFX/Glados_Number_${padNumber(ones)}.mp3`);
+        audio.onended = callback;
+      }
+    }
+    audio.onended = () => {
+      const unitAudio = new Audio(`SFX/Glados_Number_${unit}.mp3`);
+      unitAudio.onended = callback; // Call the callback after the unit sound has ended
+      unitAudio.play();
+    };
+    audio.play();
+  };
+
+  const playIntroSound = (callback) => {
+    const introAudio = new Audio(`SFX/Glados_NumberLine_Start_1.mp3`);
+    introAudio.onended = callback;
+    introAudio.play();
+  };
+
+  const playOutroSound = () => {
+    const outroAudio = new Audio(`SFX/Glados_NumberLine_End_1.mp3`);
+    outroAudio.play();
+  };
+
+  const playTimeSound = () => {
+    playIntroSound(() => {
+      if (hours) {
+        playTimeUnitSound(parseInt(hours, 10), "Hours", () => {
+          if (minutes) {
+            playTimeUnitSound(parseInt(minutes, 10), "Minutes", () => {
+              if (seconds) {
+                playTimeUnitSound(
+                  parseInt(seconds, 10),
+                  "Seconds",
+                  playOutroSound
+                );
+              } else {
+                playOutroSound();
+              }
+            });
+          } else if (seconds) {
+            playTimeUnitSound(parseInt(seconds, 10), "Seconds", playOutroSound);
+          } else {
+            playOutroSound();
+          }
+        });
+      } else if (minutes) {
+        playTimeUnitSound(parseInt(minutes, 10), "Minutes", () => {
+          if (seconds) {
+            playTimeUnitSound(parseInt(seconds, 10), "Seconds", playOutroSound);
+          } else {
+            playOutroSound();
+          }
+        });
+      } else if (seconds) {
+        playTimeUnitSound(parseInt(seconds, 10), "Seconds", playOutroSound);
+      } else {
+        playOutroSound(); // If no time units are specified, directly play the outro
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <FaceLayersContainer>
+        <FaceBackImage src="face-back.png" alt="Face Back" />
+        <OuterFaceLayersContainer>
+          <InnerFaceLayersContainer>
+            <FaceInnerBackImage
+              src="face-inner-back.png"
+              alt="Face Inner Back"
+            />
+          </InnerFaceLayersContainer>
+          <FaceInnerCoverImage
+            src="face-inner-cover.png"
+            alt="Face Inner Cover"
+          />
+          <InnerFaceLayersContainer>
+            <FaceInnerFrontImage
+              src="face-inner-front.png"
+              alt="Face Inner Front"
+            />
+          </InnerFaceLayersContainer>
+          <FaceInnerFrontCoverImage
+            src="face-inner-front-cover.png"
+            alt="Face Inner Front Cover"
+          />
+        </OuterFaceLayersContainer>
+        <FaceOuterCoverImage
+          src="face-outer-cover.png"
+          alt="Face Outer Cover"
+        />
+      </FaceLayersContainer>
+      {/* <VStack spacing={4}>
+        <Input
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+          placeholder="Enter hours (0-99)"
+        />
+        <Input
+          value={minutes}
+          onChange={(e) => setMinutes(e.target.value)}
+          placeholder="Enter minutes (0-59)"
+        />
+        <Input
+          value={seconds}
+          onChange={(e) => setSeconds(e.target.value)}
+          placeholder="Enter seconds (0-59)"
+        />
+        <Button onClick={playTimeSound}>Play Time</Button>
+      </VStack> */}
+    </AppContainer>
   );
 }
 
---
+++ src/animationAndImageFormatting.js
@@ -0,0 +1,153 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const moveUpDownLeftRight = keyframes`
+  0%, 100% {
+    transform: translateY(0) translateX(0);
+  }
+  
+  10% {
+    transform: translateY(-15px) translateX(30px); // Moves up and right
+  }
+
+  20% {
+    transform: translateY(-30px) translateX(0); // Moves up
+  }
+  
+  30% {
+    transform: translateY(-15px) translateX(-30px); // Moves up and left
+  }
+
+  40% {
+    transform: translateY(0) translateX(0); // Moves back to original position
+  }
+
+  50% {
+    transform: translateY(15px) translateX(30px) rotate(5deg); // Moves down and right
+  }
+
+  60% {
+    transform: translateY(30px) translateX(0); // Moves down
+  }
+
+  70% {
+    transform: translateY(15px) translateX(-30px) rotate(-5deg); // Moves down and left
+  }
+
+  80% {
+    transform: translateY(0) translateX(0); // Moves back to original position
+  }
+
+  90% {
+    transform: translateY(-15px) translateX(30px); // Moves up and right
+  }
+`;
+
+const outerFaceMove = keyframes`
+  0%, 40%, 100% {
+    transform: translateY(0px);
+  }
+  
+  50%, 90% {
+    transform: translateY(20px);
+  }
+`;
+
+const innerFaceMove = keyframes`
+0%, 10%, 100% {
+  transform: translateY(-30px);
+}
+
+20%, 30%, 70%, 80% {
+  transform: translateY(30px);
+}
+
+40%, 50%, 60%, 90% {
+  transform: translateY(0px);
+}
+`;
+
+export const FaceLayersContainer = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  height: 60vh; // using viewport height to match the child images
+  width: 60vh; // assuming the images are square, adjust if not
+  animation: ${moveUpDownLeftRight} 40s infinite; // Adjust duration and iteration count as needed
+`;
+
+export const OuterFaceLayersContainer = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  height: 60vh; // using viewport height to match the child images
+  width: 60vh; // assuming the images are square, adjust if not
+  animation: ${outerFaceMove} 14s infinite; // Adjust duration and iteration count as needed
+`;
+export const InnerFaceLayersContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 60vh; // using viewport height to match the child images
+  width: 60vh; // assuming the images are square, adjust if not
+  animation: ${innerFaceMove} 8s infinite; // Adjust duration and iteration count as needed
+`;
+
+export const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FaceBackImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%;
+`;
+
+export const FaceInnerBackImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%; // same height for alignment
+`;
+
+export const FaceInnerCoverImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%;
+`;
+
+export const FaceInnerFrontImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%;
+`;
+
+export const FaceInnerFrontCoverImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%;
+`;
+
+export const FaceOuterCoverImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100%;
+`;
